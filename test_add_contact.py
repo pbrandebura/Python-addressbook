ae344958b7fb26f8a635ceaa2e37348eced479af
Patryk Brandebura <p.brandebura@gmail.com>
2019-09-05 21:12:01 +0200

    First tests for group and contacts. Added fixture to run on pytest framework

---
+++ test_add_contact.py
@@ -0,0 +1,68 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from contact import Contact
+import unittest
+
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+
+    def tearDown(self):
+        self.wd.quit()
+
+    def test_test_add_contact(self):
+        wd = self.wd
+        self.login(wd, "admin", "secret")
+        self.create_contact(wd, Contact(firstname="Stachu", middlename="Jezyna", lastname="Kowal"))
+        self.logout(wd)
+
+    def test_test_add_empty_contact(self):
+        wd = self.wd
+        self.login(wd, "admin", "secret")
+        self.create_contact(wd, Contact(firstname="", middlename="", lastname=""))
+        self.logout(wd)
+
+    def tearDown(self):
+        self.wd.quit()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd, user, password):
+        self.open_home_page(wd)
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(user)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def create_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_home_page(wd)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_add_group.py
@@ -0,0 +1,22 @@
+import pytest
+from group import Group
+from application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("sdfsd", "sdf", "sdf"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
