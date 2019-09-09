8a3c693b90b8c205daf0ea2fef2cb212c068a409
Patryk Brandebura <p.brandebura@gmail.com>
2019-09-09 20:09:54 +0200

    Added test_del_first_group

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,4 @@
 from model.contact import Contact
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_test_add_contact(app):
