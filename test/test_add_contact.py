164221689378ed8a637848b351e350ec5821b7bd
Patryk Brandebura <p.brandebura@gmail.com>
2020-01-30 16:44:10 +0100

    Improvements for fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_test_add_contact(app):
-    app.session.login("admin", "secret")
     app.contact.create(Contact(firstname="Stachu", middlename="Jezyna", lastname="Kowal"))
-    app.session.logout()
 
 
 def test_test_add_empty_contact(app):
-    app.session.login("admin", "secret")
     app.contact.create(Contact(firstname="", middlename="", lastname=""))
-    app.session.logout()
