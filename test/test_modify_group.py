1eddd91e4aadeae3feeec92e79793ebf3bee330b
Patryk Brandebura <p.brandebura@gmail.com>
2020-02-01 13:12:59 +0100

    Assertion for group lists added in adding and modying tests for groups

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,23 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="newHroup"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="sdfsd"))
+    group = Group(name="sdfsd")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="newHroup"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="sdf"))
+    group = Group(header="sdf")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
