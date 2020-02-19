b5d6058abcee91cb433e48aedefc4af7fc985dcd
Patryk Brandebura <p.brandebura@gmail.com>
2020-02-19 16:52:02 +0100

    Randomization for adding contacts test

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,22 @@
+import string
+import random
+
+import pytest
+
 from model.contact import Contact
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+
+testdata = [Contact(firstname="", lastname="")] + [
+    Contact(firstname=random_string("firstanme", 10), lastname=random_string("lastname", 20))
+    for i in range(5)
+]
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Gen", lastname="Kowal")
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", lastname="")
-    contact.full_name = contact.lastname + contact.firstname
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
