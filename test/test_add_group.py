ce960c9faf229dcf821b93eef6cdbeae7b7af43d
Patryk Brandebura <p.brandebura@gmail.com>
2020-02-25 18:53:18 +0100

    Correcting fixture

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,7 @@
-import string
-import random
+
 import pytest
-
+from data.add_group import testdata
 from model.group import Group
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20),
-          footer=random_string("footer", 15))
-    for i in range(5)
-]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
