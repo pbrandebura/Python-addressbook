660dd02fb03a4a729716bb5b4800ef25326d5cf7
Patryk Brandebura <p.brandebura@gmail.com>
2020-02-26 11:57:02 +0100

    Load testbed from json file for contacts and groups

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="", lastname="", middlename="")
+    Contact(firstname="firstname1", lastname="lastname1", middlename="middlename1")
+]
